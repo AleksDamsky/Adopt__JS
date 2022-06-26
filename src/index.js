6854c76b48529d77b6293bdd9f34ba8ac50c0666
Александр Александрович Хрипунов <99286602+AleksDamsky@users.noreply.github.com>
2022-06-26 15:57:01 +0300

    setting front face

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import './CSS/style.css';
 
-console.log("LOL");
+const form = document.getElementById('form');
+const addBtn = document.getElementById('btn1');
+const addInput = form.querySelector('#question'); // можно не запрашивать глобально, а найти необходимый элемент через родителя
