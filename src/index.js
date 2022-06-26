ff70ab214a5ca94dc9ec5d65799ea078bac8c577
Александр Александрович Хрипунов <99286602+AleksDamsky@users.noreply.github.com>
2022-06-26 17:30:34 +0300

    first display output

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
+import { isValid } from 'ipaddr.js';
 import './CSS/style.css';
 
 const form = document.getElementById('form');
 const addBtn = document.getElementById('btn1');
 const addInput = form.querySelector('#question'); // можно не запрашивать глобально, а найти необходимый элемент через родителя
+
+form.addEventListener('submit', submitFormHendler); //"submit" - это нативное событие, которое отслеживает нажати на кнопку мыши или на Enter;
+
+function submitFormHendler(event){
+    event.preventDefault();
+
+    if(isValid(addInput.value)){
+        let getObject = {
+            text : addInput.value.trim(), // .trim() -  это свойсство, которое удаляет лишние пробелы.
+            date : new Date().toJSON()
+        }
+
+        addBtn.disabled = true;
+
+        addInput.value = '';
+        addInput.className = '';
+
+        addBtn.disabled = false;
+
+        console.log('question', getObject);
+    }
+}
